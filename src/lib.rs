04a86f467a2d838af125bda9c3e82954c57a75e9
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-05-16 13:36:57 -0500

    added lib.rs with main function stump, calling that from main.rs

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub fn branch_transfer() {
+    println!("Hello from library!")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use branchtransfer::branch_transfer;
+
 fn main() {
     println!("Hello, world!");
+    branch_transfer();
 }
